39b63921b42f10830cd94a905d11216e36ee7a07
Reynolito Reso-or <reynolitoresoor123@gmail.com>
2024-11-07 09:43:53 +0800

    Update config

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'/peer-group-well-being-app/session.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/moon-noon-app/session.php');
 
-if(!defined('base_url')) define('base_url','http://localhost/peer-group-well-being-app/');
-if(!defined('BASE_APP')) define('BASE_APP', $_SERVER['DOCUMENT_ROOT'].'/peer-group-well-being-app/' );
+if(!defined('base_url')) define('base_url','http://localhost/moon-noon-app/');
+if(!defined('BASE_APP')) define('BASE_APP', $_SERVER['DOCUMENT_ROOT'].'/moon-noon-app/' );
 if(!defined('DB_SERVER')) define('HOST',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
